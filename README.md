f449450f2ca1d3c3e6274c43c4f5d14b97f73f61
Felipe Alcântara <FelipeTheFeh@gmail.com>
2024-04-17 02:50:57 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Aprendendo-logica-de-programacao-de-listas-em-python
+ Este algoritmo é um exemplo simples de como manipular listas e tuplas em Python.
---
+++ Reordenando Dias da Semana.py
@@ -0,0 +1,11 @@
+# Declaração de uma tupla 'indice' com os valores (1, 2, 3, 0, 4, 5)
+indice = (1, 2, 3, 0, 4, 5)
+
+# Declaração de uma lista 'semana' com os valores ["seg", "ter", "qua", "dom", "sex", "sab"]
+semana = ["seg", "ter", "qua", "dom", "sex", "sab"]
+
+# Loop 'for' que percorre cada valor em 'indice'
+for i in indice:
+    # Usa o valor atual de 'i' como índice para acessar o elemento correspondente em 'semana'
+    # Imprime o dia da semana correspondente
+    print (semana[i])
